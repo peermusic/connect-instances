622bd794ab16ac64eb34e0297cb8ccc777301cad
Per Guth <mail@perguth.de>
2016-01-05 14:55:01 +0100

    added an example

--- example.js
+++ example.js
@@ -1,3 +1,18 @@
 var Connect = require('./index.js')
+// window.localStorage.debug = ''
 
-window.c = new Connect(null, null, ['localhost:7000'])
+var a = new Connect(null, null, ['localhost:7000'])
+var b = new Connect(null, null, ['localhost:7001'])
+
+var invite = a.issueInvite('localhost:7001')
+
+b.receiveInvite(invite[1])
+
+b.metaSwarm.on('data', function (data) { console.log(data) })
+
+setTimeout(function () {
+  a.metaSwarm.send('hello I am a! Who are you?')
+}, 500)
+
+window.a = a
+window.b = b
