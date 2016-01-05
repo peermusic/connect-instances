95be53d8a8419de23c5108900feb004ff8fe9594
Per Guth <mail@perguth.de>
2016-01-05 14:42:11 +0100

    invite/receive logic working

--- index.js
+++ index.js
@@ -12,13 +12,21 @@ function Connect (keyPair, peers, hubs, opts) {
   if (!(this instanceof Connect)) return new Connect(opts)
   if (!opts) opts = {}
 
-  this.events = opts.events || []
   this.hubs = hubs || []
+  if (!keyPair) {
+    keyPair = nacl.box.keyPair()
+    keyPair = {
+      secretKey: nacl.util.encodeBase64(keyPair.secretKey),
+      publicKey: nacl.util.encodeBase64(keyPair.publicKey)
+    }
+  }
+  this.keyPair = keyPair
   this.issuedInvites = opts.issuedInvites || []
-  this.keyPair = keyPair || nacl.box.keyPair()
-  this.namespace = opts.namespace || 'peermusic'
-  this.opts = opts.opts || {}
   this.receivedInvites = opts.receivedInvites || {}
+
+  this.opts = opts.opts || {}
+  this.opts.keyPair = this.keyPair
+  this.opts.namespace = opts.namespace || 'peermusic'
 
   this._init()
 }
@@ -26,50 +34,64 @@ function Connect (keyPair, peers, hubs, opts) {
 Connect.prototype._init = function () {
   var events = ['connect', 'disconnect']
   Object.assign(this.opts, {events})
-  this.swarm = new Meta(swarm, this.hubs, this.opts)
-  if (!this.swarm.opts.issuedInvites) this.swarm.opts.issuedInvites = []
-  if (!this.swarm.opts.receivedInvites) this.swarm.opts.receivedInvites = {}
+
+  this.metaSwarm = new Meta(swarm, this.hubs, this.opts)
+
+  if (!this.metaSwarm.opts.issuedInvites) {
+    this.metaSwarm.opts.issuedInvites = []
+  }
+  if (!this.metaSwarm.opts.receivedInvites) {
+    this.metaSwarm.opts.receivedInvites = {}
+  }
 }
 
 Connect.prototype.issueInvite = function (hub) {
   if (!hub) throw new Error('hub needed')
-  hub = hub.indexOf('://') === -1 ? 'http://' + hub : hub
+
   var signKeyPair = nacl.sign.keyPair()
   var signPrivKey = nacl.util.encodeBase64(signKeyPair.secretKey)
   var signPubKey = nacl.util.encodeBase64(signKeyPair.publicKey)
   // var myPubKey   = nacl.util.encodeBase64(o2a(keyPair.publicKey))
-  var myPubKey = nacl.util.encodeBase64(signKeyPair.publicKey)
+  var myPubKey = this.keyPair.publicKey
 
   if (this.hubs.indexOf(hub) === -1) {
-    this.swarm.addHub(hub)
+    this.metaSwarm.addHub(hub)
     this.hubs.push(hub)
   }
-  this.swarm.opts.issuedInvites.push(signPubKey)
+
   this.issuedInvites.push(signPubKey)
-  return [signPubKey, hub + '/#' + myPubKey + ':' + signPrivKey]
+  this.metaSwarm.swarms[hub].issuedInvites.push(signPubKey)
+
+  return [signPubKey,
+    'web+peermusic://' + hub + '/#' + myPubKey + ':' + signPrivKey]
 }
 
 Connect.prototype.receiveInvite = function (uri) {
   function parseInvite (uri) {
-    // format: http://host:port/#boxPubkey:signPrivKey
+    // format: web-peermusic://host:port/#boxPubkey:signPrivKey
     var parts = uri.split('/#', 2)
-    var hub = parts[0]
+    var hub = parts[0].substr(parts[0].indexOf('://') + 3)
     parts = parts[1].split(':', 2)
     var pubKey = parts[0]
     var signPrivKey = parts[1]
+
     return [hub, pubKey, {[pubKey]: signPrivKey}]
   }
+
   var invite = parseInvite(uri)
 
-  if (this.receivedInvites[invite[1]]) {
-    debug('already received invite')
-    return
-  }
-  this.receivedInvites = Object.assign(this.receivedInvites, invite[2])
   if (this.hubs.indexOf(invite[0]) === -1) {
-    this.swarm.opts.receivedInvites = this.receivedInvites
-    this.swarm.addHub(invite[0])
+    debug('skipping already known hub')
+    this.metaSwarm.addHub(invite[0])
     this.hubs.push(invite[0])
   }
+  if (this.receivedInvites[invite[1]]) {
+    debug('we already received an invite from that peer but continue')
+  }
+
+  debug('adding new invite', invite[2])
+  this.receivedInvites = Object.assign(this.receivedInvites, invite[2])
+  this.metaSwarm.swarms[invite[0]].receivedInvites = this.receivedInvites
+
   return invite
 }
